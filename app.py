2097b3c7a9f3188216a73f1da7998b2a87a11c86
Arya <nairaishwarya48@gmail.com>
2023-10-24 14:29:34 +0530

    Added database and core functionality.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_sqlalchemy  import SQLAlchemy
+from flask import Flask, render_template, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
@@ -12,18 +12,40 @@ class Todo(db.Model):
     no = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime,default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self)->str:
+    def __repr__(self) -> str:
         return f"{self.no} - {self.title}"
 
-@app.route("/")
+    # converting to dict so that it can be easily written in JSON
+    def to_dict(self):
+        return {
+            'no': self.no,
+            'title': self.title,
+            'desc': self.desc,
+            'date_created': self.date_created.isoformat()
+        }
+
+
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
-    todo=Todo(title="First Todo Task", desc="Start diving into blockchain security")
+    if request == 'POST':
+        print("Post")
+    todo = Todo(title="First Todo",
+                desc="Start blockchain security")
     db.session.add(todo)
     db.session.commit()
-    return render_template("index.html")
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo=allTodo)
     # return "Hello, World!"
 
+
+@app.route("/show")
+def products():
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return jsonify(allTodo.to_dict())
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
